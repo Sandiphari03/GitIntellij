a15fecdf450fe261338ef7a71647199f163efd83
Sandip Hari <94623134+Sandiphari03@users.noreply.github.com>
2025-10-16 20:36:11 +0530

    Remove loop and add variable declaration
    Replaced loop with a variable declaration and print statement.

--- src/main/java/org/github/sandip/Main.java
+++ src/main/java/org/github/sandip/Main.java
@@ -14,9 +14,9 @@ public class Main {
         System.out.println("git merge commit");
         System.out.println("My first user registration commit branch");
         System.out.println("My first patient registration commit branch");
-        for(int i=0;i<=10;i++){
-            System.out.println(i);
-        }
+       int a =10;
+        System.out.println(a);
+    }
 
 
 
